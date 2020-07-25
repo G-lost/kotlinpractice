977a236c34e1196aeb7e189e159fab3dc4040964
Glost <gjf734128912@gmail.com>
2020-07-25 16:19:27 +0800

    2ndday

--- app/src/main/java/com/glost/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/glost/kotlinpractice/MainActivity.kt
@@ -2,6 +2,8 @@ package com.glost.kotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.mainlayout.*
@@ -11,6 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainlayout)
         button1.setOnClickListener { Toast.makeText(this, "you click button1", Toast.LENGTH_SHORT).show() }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mainmenu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.setting -> Toast.makeText(this, "setting option", Toast.LENGTH_SHORT).show()
+            R.id.add_item -> Toast.makeText(this, "add option", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "remove option", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
--- app/src/main/java/com/glost/kotlinpractice/firstkotlin.kt
+++ app/src/main/java/com/glost/kotlinpractice/firstkotlin.kt
@@ -8,8 +8,8 @@ fun main()
 
     val fruit = listOf("apple", "watermelon", "banana", "orange", "pear", "grape")
     val maxLengthFruit = fruit.maxBy { it.length }
-    
+    val newFruit = fruit.filter { it.length<=5 }.map { it.toUpperCase() }
     println( "max length fruit is $maxLengthFruit'" )
-
+    println("result of filter is $newFruit")
 
 }
