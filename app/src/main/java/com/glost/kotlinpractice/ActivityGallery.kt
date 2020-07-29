a9652bd68ea031f0cd0734958cbdc937fc3a0ff2
Glost <gjf734128912@gmail.com>
2020-07-29 20:27:29 +0800

    grid layout test

--- app/src/main/java/com/glost/kotlinpractice/ActivityGallery.kt
+++ app/src/main/java/com/glost/kotlinpractice/ActivityGallery.kt
@@ -2,9 +2,11 @@ package com.glost.kotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_setting.*
+import kotlin.random.Random
 
 class ActivityGallery : AppCompatActivity() {
     private val imageList = ArrayList<Image>()
@@ -13,7 +15,8 @@ class ActivityGallery : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
         initFruits()
-        val layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+        val layoutManager = GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false)
+//        val layoutManager = StaggeredGridLayoutManager(3, GridLayoutManager.VERTICAL)
         recyclerView.layoutManager = layoutManager
         val adapter = GalleryAdapter(imageList)
         recyclerView.adapter = adapter
@@ -21,17 +24,26 @@ class ActivityGallery : AppCompatActivity() {
     }
 
     private fun initFruits(){
-        repeat(2) {
-            imageList.add(Image("Apple", R.drawable.apple_pic))
-            imageList.add(Image("Banana", R.drawable.banana_pic))
-            imageList.add(Image("Orange", R.drawable.orange_pic))
-            imageList.add(Image("Watermelon", R.drawable.watermelon_pic))
-            imageList.add(Image("Pear", R.drawable.pear_pic))
-            imageList.add(Image("Grape", R.drawable.grape_pic))
-            imageList.add(Image("Pineapple", R.drawable.pineapple_pic))
-            imageList.add(Image("Strawberry", R.drawable.strawberry_pic))
-            imageList.add(Image("Cherry", R.drawable.cherry_pic))
-            imageList.add(Image("Mango", R.drawable.mango_pic))
+        repeat(3) {
+            imageList.add(Image(getRandomLengthString("Apple"), R.drawable.apple_pic))
+            imageList.add(Image(getRandomLengthString("Banana"), R.drawable.banana_pic))
+            imageList.add(Image(getRandomLengthString("Orange"), R.drawable.orange_pic))
+            imageList.add(Image(getRandomLengthString("Watermelon"), R.drawable.watermelon_pic))
+            imageList.add(Image(getRandomLengthString("Pear"), R.drawable.pear_pic))
+            imageList.add(Image(getRandomLengthString("Grape"), R.drawable.grape_pic))
+            imageList.add(Image(getRandomLengthString("Pineapple"), R.drawable.pineapple_pic))
+            imageList.add(Image(getRandomLengthString("Strawberry"), R.drawable.strawberry_pic))
+            imageList.add(Image(getRandomLengthString("Cherry"), R.drawable.cherry_pic))
+            imageList.add(Image(getRandomLengthString("Mango"), R.drawable.mango_pic))
         }
+    }
+
+    private fun getRandomLengthString(string: String): String {
+        val times = (1..15).random()
+        val builder = StringBuilder()
+        repeat(times){
+            builder.append(string)
+        }
+        return builder.toString()
     }
 }
